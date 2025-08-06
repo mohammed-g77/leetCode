ff0db25e3a96ceabb9e1947f05d5335b8949d5b4
Mohammed O Ghanem <146705430+mohammed-g77@users.noreply.github.com>
2025-08-06 15:33:51 +0300

    Time: 4 ms (37.05%) | Memory: 15.6 MB (50.75%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void backtrack(vector<string>& res, string s, int open, int close, int n) {
+        if (s.length() == 2 * n) {
+            res.push_back(s);
+            return;
+        }
+        if (open < n) backtrack(res, s + '(', open + 1, close, n);
+        if (close < open) backtrack(res, s + ')', open, close + 1, n);
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        backtrack(res, "", 0, 0, n);
+        return res;
+    }
+
+};
